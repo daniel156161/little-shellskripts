58758eb4694d5f7fcf0eb6c04960f955d92c4b98
daniel156161 <d.dolezal97@protonmail.com>
2020-11-06 17:59:57 +0100

    New git.sh code

--- git.sh
+++ git.sh
@@ -4,10 +4,24 @@
 #Write Date: 22.10.2020
 #Use: Easy git
 
-if [ ! -z $1 ]; then
-	git add -A
-	git commit -m "$1"
-	git push
+folder=$1
+massage=$2
+
+if [ ! -z $folder ]; then
+ echo "$folder : $massage"
+ if [ -d $folder/.git ]; then
+  cd $folder
+  #Add all Files
+  echo "Added all changed Files"
+  git add -A
+  #Commit
+  git commit -m "$massage"
+  #Upload all Files
+  echo "Upload files to github"
+  git push
+ else
+  echo "No Git Repo"
+ fi
 else
-	echo "No Commit Massage, USE $0 'Massage'"
+  echo "USE: $0 folder 'Commit Text like this' "
 fi
